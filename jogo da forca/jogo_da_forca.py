cecd981e7465cceeea11ed76a9b83e353993be4b
Ingrid Machado <129779632+MachIngrid@users.noreply.github.com>
2023-07-25 15:52:35 -0300

    Add files via upload

---
+++ jogo da forca/jogo_da_forca.py
@@ -0,0 +1,137 @@
+import random
+def jogar():
+    
+    print("""***** Bem vindo ao jogo da Forca!*****
+       ________
+    //        |
+    |         O
+    |        /|\\
+    |        / \\
+    |
+    \\_________""")
+
+
+    arquivo = open('palavra_frutas.txt','r')
+    palavras = []
+
+    for line in arquivo:
+       linha = line.strip()
+       palavras.append(linha)
+
+    arquivo.close()  
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    
+
+    letras_acertadas=['_' for letra in palavra_secreta] # Adapta a quantidade de '_' para a palavra escolhida
+    letras_faltando = str(letras_acertadas.count('_')) 
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print("Palavra:",*letras_acertadas)
+    while(not acertou and not enforcou):
+        
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
+        
+        index = 0
+
+        if(chute in palavra_secreta):
+
+         for letra in palavra_secreta:
+
+            if(chute.upper() == letra.upper()):
+                print(f"Encontrei a letra{letra} na posição {index}")
+                letras_acertadas[index] = letra
+                letras_faltando = str(letras_acertadas.count('_'))
+
+            index = index+1   
+
+        else:
+           erros +=1     
+
+        enforcou = erros == 6
+
+        if erros==1:
+           
+           print(""" 
+           ________
+         //        |
+         |         O
+         |        
+         |        
+         |
+         \\_________""")
+           
+        elif erros == 2:
+           
+           print("""
+           ________
+         //        |
+         |         O
+         |         |
+         |        
+         |
+         \\_________""")
+        
+        elif erros == 3:
+           
+           print(""" 
+            ________
+         //        |
+         |         O
+         |        /|
+         |        
+         |
+         \\_________""")
+           
+        elif erros == 4:
+           
+           print("""  
+            ________
+         //        |
+         |         O
+         |        /|\\
+         |         
+         |
+         \\_________""")   
+           
+        elif erros == 5:
+           
+           print("""   
+            ________
+         //        |
+         |         O
+         |        /|\\
+         |        / 
+         |
+         \\_________""") 
+
+           
+           
+
+
+        acertou = '_' not in letras_acertadas 
+        print("Palavra: ",*letras_acertadas)
+        print( 'Ainda faltam acertar {} letras'.format(letras_faltando))
+        print ("Jogando...")
+        
+    if acertou:
+       print("****** Parabéns Voçe ganhou ******")
+    elif enforcou:
+       print("""
+           ****** Você Perdeu!! ******
+       ________
+       //        |
+       |         O
+       |        /|\\
+       |        / \\
+       |
+       \\_________
+          """)    
+    print("Fim do jogo")
+
+if (__name__=="__main__"):   
+    jogar()
